dda6e4d1c301fb2240be77818b569ad3df39786e
Moh3A <32433254+moh3a@users.noreply.github.com>
2022-11-21 21:12:39 +0100

    Create remap.lua

---
+++ .config/nvim/lua/moh3a/remap.lua
@@ -0,0 +1,11 @@
+
+local nnoremap = require("moh3a.keymap").nnoremap
+
+nnoremap("<leader>tt", "<cmd>NvimTreeToggle<cr>")
+nnoremap("<leader>tf", "<cmd>NvimTreeFocus<cr>")
+
+-- Find files using Telescope command-line sugar.
+nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
+nnoremap("<leader>fg", "<cmd>Telescope live_grep<cr>")
+nnoremap("<leader>fb", "<cmd>Telescope buffers<cr>")
+nnoremap("<leader>fh", "<cmd>Telescope help_tags<cr>")
